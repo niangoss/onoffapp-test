3bef7fcf4489b09bf64c4fdcee175905c5f8c466
Aliou NIANG <aliou.niang@colis-gp.com>
2021-06-25 14:11:30 +0200

    Add Meterial UI

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Calls from "./containers/Calls";
+import Container from "@material-ui/core/Container";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Route exact path="/" component={Calls}/>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Container>
+                <Router>
+                    <Route exact path="/" component={Calls}/>
+                </Router>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
--- src/components/CallList.js
+++ src/components/CallList.js
@@ -2,23 +2,74 @@ import React from 'react';
 import PropTypes from "prop-types";
 import moment from "moment";
 
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import Container from "@material-ui/core/Container";
+import PersonIcon from '@material-ui/icons/Person';
+
 function formateDate(date) {
-    if (moment(date).isSame(moment(), 'day'))
+    if (moment(date).utc().isSame(moment(), 'day'))
         return 'Today';
-    else if (moment(date).isSame(moment().add(-1, 'days'), 'day'))
+    else if (moment(date).utc().isSame(moment().add(-1, 'days'), 'day'))
         return 'Yesterday';
-    return moment(date).format("MMMM Do")
+    return moment(date).utc().format("MMMM Do")
 }
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        maxWidth: '36ch',
+        backgroundColor: theme.palette.background.paper,
+    },
+    inline: {
+        display: 'inline',
+    },
+}));
 
 export default function CallList({ items }) {
     items.sort((left, right) => moment.utc(right.date).diff(moment.utc(left.date)));
 
+    const classes = useStyles();
+
+
     return (
-        <ul>
+        <List className={classes.root}>
             {items.map((item) => (
-                <li key={item.id}>Number: {item.phoneNumber}, Date: {formateDate(item.date)}</li>
+                <Container key={item.id}>
+                    <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                            <Avatar>
+                                <PersonIcon />
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary={item.phoneNumber}
+                            secondary={
+                                <React.Fragment>
+                                    <Typography
+                                        component="span"
+                                        variant="body2"
+                                        className={classes.inline}
+                                        color="textPrimary"
+                                    >
+                                        {item.phoneType}
+                                    </Typography>
+                                    - {formateDate(item.date)} - {moment(item.date).utc().format('hh:mm A')}
+                                </React.Fragment>
+                            }
+                        />
+                    </ListItem>
+                    <Divider variant="inset" component="li" />
+                </Container>
             ))}
-        </ul>
+        </List>
     );
 
 }
